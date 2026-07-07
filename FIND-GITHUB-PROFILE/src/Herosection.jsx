a6b58c3789622124491d7313daf3023b8c60212b
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-07-07 23:57:09 +0530

    mc

--- FIND-GITHUB-PROFILE/src/Herosection.jsx
+++ FIND-GITHUB-PROFILE/src/Herosection.jsx
@@ -2,6 +2,26 @@ function Herosection(){
 
     return(
         <>
+        <section className="homepage">
+
+
+            
+            <div className="input-box">
+                <input type="text" placeholder="Enter username"/> 
+                <button>SEARCH</button>
+
+
+
+
+
+            </div>
+            <div className="main-box" >
+                
+
+            </div>
+
+
+        </section>
         
         </>
 
