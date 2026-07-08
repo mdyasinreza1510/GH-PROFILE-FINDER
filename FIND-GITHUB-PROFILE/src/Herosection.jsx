fdf2470d4ae7337953c982798fbe19333fe59401
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-07-08 23:35:35 +0530

    layout done

--- FIND-GITHUB-PROFILE/src/Herosection.jsx
+++ FIND-GITHUB-PROFILE/src/Herosection.jsx
@@ -14,9 +14,12 @@ function Herosection(){
 
 
             <div className="main-box" >
-                <div className="cards" id="card1"></div>
-                <div className="cards" id="card2"></div>
-                <div className="cards" id="card3"></div>
+                <div className="cards" id="left"></div>
+                <div className="cards" id="right">
+                    <div className="b1"></div>
+                    <div className="b2"></div>
+                </div>
+                
                 
 
             </div>
