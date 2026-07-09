2554dfb7613ead05f73cff3fbbad7ff80fb3a08c
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-07-09 23:46:01 +0530

    right side done

--- FIND-GITHUB-PROFILE/src/Herosection.jsx
+++ FIND-GITHUB-PROFILE/src/Herosection.jsx
@@ -21,9 +21,33 @@ function Herosection(){
                     <div className="details"></div>
                     <button>vuew on github</button>
                 </div>
+
+
+
                 <div className="cards" id="right">
-                    <div className="b1"></div>
-                    <div className="b2"></div>
+                    <div className="b1">
+                        <div className="mini-boxes" id="r1"></div>
+                        <div className="mini-boxes" id="r2"></div>
+                        <div className="mini-boxes" id="r3"></div>
+                        <div className="mini-boxes" id="r4"></div>
+                    </div>
+
+
+
+                    <div className="b2">
+                        <div className="d1">
+                            <span>repositories</span>
+                            <button>view all</button>
+                        </div>
+
+                        <div className="repo-main-box">
+                            <div className="repobox"></div>
+                            <div className="repobox"></div>
+                            <div className="repobox"></div>
+                            <div className="repobox"></div>
+                            <div className="repobox"></div>
+                        </div>
+                    </div>
                 </div>
                 
                 
