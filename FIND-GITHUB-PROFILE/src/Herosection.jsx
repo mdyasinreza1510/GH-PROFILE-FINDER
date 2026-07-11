7027dae0f00762320381ee0d06ac6244d2850853
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-07-11 23:43:36 +0530

    adding userlogics

--- FIND-GITHUB-PROFILE/src/Herosection.jsx
+++ FIND-GITHUB-PROFILE/src/Herosection.jsx
@@ -1,80 +1,88 @@
 import { useState } from "react";
 import "./Herosection.css"
-function Herosection(){
-    let base_url="https://api.github.com/users/"
-    const [username,setusername]=useState("");
-    const [userdata,setuserdata]=useState(null);
-    const[loading,setloading]=useState(false);
-    const[error,seterror]=useState("");
+function Herosection() {
+    let base_url = "https://api.github.com/users/"
+    const [username, setusername] = useState("");
+    const [userdata, setuserdata] = useState(null);
+    const [loading, setloading] = useState(false);
+    const [error, seterror] = useState("");
 
-    function usernamechange(event){
+    function usernamechange(event) {
         setusername(event.target.value)
     }
-    const display = async()=>{
-        const response= await fetch(`${base_url}${username}`);
-        const data= await response.json();
-        console.log (data);
-        setuserdata(data);
+    const display = async () => {
+        if (username == "") {
+            seterror("please enter a github username");
+        }
+        else {
+            setloading(true);
+            const response = await fetch(`${base_url}${username}`);
+            const data = await response.json();
+            setloading(false);
+            console.log(data);
+            setuserdata(data);
+        }
+
     }
 
-    return(
+    return (
         <>
-        <section className="homepage">
+            <section className="homepage">
 
 
-            
-            <div className="input-box">
-                <input onChange={usernamechange} type="text" placeholder="Enter username" value={username}/> 
-                <button onClick={display}>SEARCH</button>
-            </div>
 
-
-            <div className="main-box" >
-                <div className="cards" id="left">
-                    <div className="pfp"><img src={userdata?.avatar_url} alt="pfp" /></div>
-
-                    <span>@{userdata?.login}</span>
-
-                    <span>{userdata?.bio}</span>
-                    <div className="details">my details</div>
-                    <span><a href={userdata?.html_url} target="_blank">view on github</a></span>
+                <div className="input-box">
+                    <input onChange={usernamechange} type="text" placeholder="Enter username" value={username} />
+                    <button onClick={display}>SEARCH</button>
                 </div>
 
 
+                <div className="main-box" >
+                    <div className="cards" id="left">
+                        <div className="pfp"><img src={userdata?.avatar_url} alt="pfp" /></div>
 
-                <div className="cards" id="right">
-                    <div className="b1">
-                        <div className="mini-boxes" id="r1"></div>
-                        <div className="mini-boxes" id="r2"></div>
-                        <div className="mini-boxes" id="r3"></div>
-                        <div className="mini-boxes" id="r4"></div>
+                        <span>@{userdata?.login}</span>
+
+                        <span>{userdata?.bio}</span>
+                        <div className="details">my details</div>
+                        <span><a href={userdata?.html_url} target="_blank">view on github</a></span>
                     </div>
 
 
 
-                    <div className="b2">
-                        <div className="d1">
-                            <span>repositories</span>
-                            <button>view all</button>
+                    <div className="cards" id="right">
+                        <div className="b1">
+                            <div className="mini-boxes" id="r1"></div>
+                            <div className="mini-boxes" id="r2"></div>
+                            <div className="mini-boxes" id="r3"></div>
+                            <div className="mini-boxes" id="r4"></div>
                         </div>
 
-                        <div className="repo-main-box">
-                            <div className="repobox"></div>
-                            <div className="repobox"></div>
-                            <div className="repobox"></div>
-                            <div className="repobox"></div>
-                            <div className="repobox"></div>
+
+
+                        <div className="b2">
+                            <div className="d1">
+                                <span>repositories</span>
+                                <button>view all</button>
+                            </div>
+
+                            <div className="repo-main-box">
+                                <div className="repobox"></div>
+                                <div className="repobox"></div>
+                                <div className="repobox"></div>
+                                <div className="repobox"></div>
+                                <div className="repobox"></div>
+                            </div>
                         </div>
                     </div>
+
+
+
                 </div>
-                
-                
-
-            </div>
 
 
-        </section>
-        
+            </section>
+
         </>
 
     );
